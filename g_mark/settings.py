ea34a73b0fb7fd0537ce2fd09a76579d980b1f61
Chaosas100 <101866007+Erikas-Ramanauskas@users.noreply.github.com>
2023-10-30 22:37:36 +0000

    Heroku deploy 2

--- g_mark/settings.py
+++ g_mark/settings.py
@@ -28,8 +28,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://milestone-project--4-cae9f77b4759.herokuapp.com/','localhost']
+ALLOWED_HOSTS = []
 
+if DEBUG:
+    ALLOWED_HOSTS += ['localhost', '127.0.0.1']
+else:
+    # '*.herokuapp.com'
+    ALLOWED_HOSTS += ['https://milestone-project--4-cae9f77b4759.herokuapp.com/','milestone-project--4-cae9f77b4759.herokuapp.com']
 
 # Application definition
 
