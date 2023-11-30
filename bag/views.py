094ad26f0d1a80333023590b01aa626cdf24a514
Chaosas100 <101866007+Erikas-Ramanauskas@users.noreply.github.com>
2023-11-30 23:33:49 +0000

    Eddited bag html

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponse
+from products.models import Product
+from django.shortcuts import get_object_or_404
 
 
 def view_bag(request):
@@ -16,3 +20,30 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+        else:
+            bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
