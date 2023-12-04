762c573bd8eed8c6e00b0b4602b903a944687001
Chaosas100 <101866007+Erikas-Ramanauskas@users.noreply.github.com>
2023-12-04 21:45:02 +0000

    created Checkout App

---
+++ checkout/models.py
@@ -0,0 +1,67 @@
+import uuid
+
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from products.models import Product
+
+# Create your models here.
+
+class Order(models.Model):
+    
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(max_length=254, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    street_address1 = models.CharField(max_length=80, blank=False)
+    street_address2 = models.CharField(max_length=80, blank=True)
+    county = models.CharField(max_length=80, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    
+    def __str__(self):
+        return self.order_number
+    
+    def _generate_order_number(self):
+        """Generate a random, unique order number using UUID"""
+        
+        return uuid.uuid4().hex.upper()
+    
+    def update_total(self):
+        """Update grand total each time a line item is added, accounting for delivery costs."""
+        
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.grand_total = self.order_total + settings.STANDARD_DELIVERY_PERCENTAGE
+        else:
+            self.grand_total = self.order_total
+    
+    def save(self, *args, **kwargs):
+        """Override the original save method to set the order number if it hasn't already been set."""
+        
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+
+class OrderLineItem(models.Model):
+    
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(blank=False)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
+    
+    def save(self, *args, **kwargs):
+        """Override the original save method to set the lineitem total and update the order total."""
+        
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
--- g_mark/settings.py
+++ g_mark/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
+    'checkout',
     
     
     # Other
