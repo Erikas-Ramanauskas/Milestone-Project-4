36a4999ef56217233b4a764b0b9f8d3963a29311
Chaosas100 <101866007+Erikas-Ramanauskas@users.noreply.github.com>
2024-02-03 23:39:42 +0000

    Fixed Meniu sizing

--- static/js/base.js
+++ static/js/base.js
@@ -14,8 +14,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
   for (let ul of ulList) {
     const li = ul.querySelectorAll("li");
-
-    const numberOfColumns = Math.ceil(li.length / 16);
+    const topPosition = 80; // procentage number for top position of drop down menu
+    const windowHeight = window.innerHeight;
+  
+    const totalHeight = windowHeight - topPosition
+    const totalNumberOfItems = Math.ceil(totalHeight / 40);
+  
+    const numberOfColumns = Math.ceil(li.length / totalNumberOfItems);
     ul.style.width = `${numberOfColumns * 200}px`;
   }
 });
